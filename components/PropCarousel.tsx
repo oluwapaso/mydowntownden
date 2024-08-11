3794e7af8e355e9af519a151c09d7f9d42b5b737
oluwapaso <adebayoishola01@gmail.com>
2024-08-11 11:07:22 +0100

    Genera update

--- components/PropCarousel.tsx
+++ components/PropCarousel.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Slider from "react-slick";
-//import 'react-multi-carousel/lib/styles.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import useCurrentBreakpoint from '@/_hooks/useMediaQuery';
-import CustomLinkMain from './CustomLinkMain';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import moment from 'moment';
